a5e0a30560176028562c39d833814a56dc970bab
Olga Goryszewska <olga.goryszewska@gmail.com>
2026-06-14 19:50:17 +1200

    push

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,5 @@
 import '../src/index.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+    return <Component {...pageProps} />;
 }
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import HomePage from './pages/HomePage';
+
+function App() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <div className="flex-grow">
+        <HomePage />
+      </div>
+    </div>
+  );
+}
+
+export default App;
