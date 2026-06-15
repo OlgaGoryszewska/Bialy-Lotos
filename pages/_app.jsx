1ca2b4761463073e9a73ecd7263310743a779c18
Olga Goryszewska <olga.goryszewska@gmail.com>
2026-06-15 12:50:47 +1200

    Add public favicon

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,15 @@
 import '../src/index.css'
+import Head from 'next/head'
 import { CookieConsentBanner } from '../src/components/PrivacyConsent'
 import { SiteFooter } from '../src/components/SiteFooter'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.png" type="image/png" />
+      </Head>
       <Component {...pageProps} />
       <SiteFooter />
       <CookieConsentBanner />
